65da74bb4851f83797bce557711747f00f0fd2bc
Lucas Batista <lucas.batista@dacta.tec.br>
2026-01-05 21:23:15 -0300

    fix: compatibility new version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
     provideRouter(routes)
   ]
 };
